ae1f8fd8559525766a62c4955b5e698a9e90df71
Dodiya VIkram <dodiyavikramsinh@gmail.com>
2023-03-15 17:21:39 +0530

    Add Demo text

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Dashboard from './components/Dashboard';
 function App() {
   return (
     <div className="App">
-      <Dashboard> </Dashboard>
+      <Dashboard>Demo</Dashboard>
     </div>
   );
 }
